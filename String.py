f01fec67221f12a345a50faaa1b8136228c9c5f6
reetika <reetikafogat@gmail.com>
2025-12-29 08:04:08 +0530

    the first string commit

---
+++ String.py
@@ -0,0 +1,21 @@
+#📍reverse a sting
+
+# str='my name is yours'
+# stack=[]
+# res=''
+# for i in str:
+#     stack.append(i)
+# for i in range(len(stack)):
+#     res+=stack.pop()
+# print(res)
+
+#📍reverse words in a string-there should be only one dot in between words all other 
+# trailing or leading dots should be removed
+
+# string='..geeks.for.mygeeks..'
+# words=[word for word in string.split('.') if word]
+# words.reverse()
+# result='.'.join(words)
+# print(result)
+
+
